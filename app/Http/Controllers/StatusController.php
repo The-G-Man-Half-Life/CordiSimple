9cfc6db03d46a6a8c2505ec637eb05ce2bca1a73
Deilons <apolo4748@gmail.com>
2024-10-28 06:54:02 -0500

    Feat: status view

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -7,30 +7,30 @@ use Illuminate\Http\Request;
 
 class StatusController extends Controller
 {
-    public function index()
+    public function index() //get all
     {
     }
 
-    public function create()
+    public function create() // create
     {
     }
 
-    public function store(Request $request)
+    public function store(Request $request) //metodo create con validaciones
     {
     }
 
-    public function show(Status $status)
+    public function show(Status $status) // get by id
     {
     }
 
-    public function edit(Status $status)
+    public function edit(Status $status) // edit con validaciones
     {
     }
-    public function update(Request $request, Status $status)
+    public function update(Request $request, Status $status) // edit pero sin validaciones
     {
     }
 
-    public function destroy(Status $status)
+    public function destroy(Status $status) // metodo delete
     {
     }
 }
---
+++ resources/js/Pages/Status.jsx
@@ -0,0 +1,12 @@
+import { Head } from '@inertiajs/react';
+
+export default function Status() {
+    return (
+        <>
+            <Head title="Status" />
+            <div className="text-center">
+                <p className="text-3xl font-bold">Status</p>
+            </div>
+        </>
+    );
+}
